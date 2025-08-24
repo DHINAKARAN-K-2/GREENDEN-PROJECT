4fcc03d79e8ce034f620834b62e42b3eb41ef9e8
DHINAKARAN-K-2 <kcsdhina2580@gmail.com>
2025-08-24 13:54:22 +0530

    Adding Humberg icon responsive

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+menuBtn.addEventListener('click', function() {
+    mobileMenu.classList.remove('-translate-x-full');
+});
+
+closeBtn.addEventListener('click', function() {
+    mobileMenu.classList.add('-translate-x-full');
+});
