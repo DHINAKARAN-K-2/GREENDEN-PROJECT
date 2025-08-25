eb005c6d856d03b478c13147cb1bf003c1ad7bf0
DHINAKARAN-K-2 <kcsdhina2580@gmail.com>
2025-08-25 22:16:08 +0530

    New Navbar Setting

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const menuBtn = document.getElementById('menu-btn');
 const closeBtn = document.getElementById('close-btn');
 const mobileMenu = document.getElementById('mobile-menu');
 menuBtn.addEventListener('click', function() {
-    mobileMenu.classList.remove('-translate-x-full');
+    mobileMenu.classList.remove('translate-x-full');
 });
 
 closeBtn.addEventListener('click', function() {
-    mobileMenu.classList.add('-translate-x-full');
+    mobileMenu.classList.add('translate-x-full');
 });
