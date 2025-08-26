629f658f913b7874240c023bc4418e544f469614
DHINAKARAN-K-2 <kcsdhina2580@gmail.com>
2025-08-26 08:09:08 +0530

    adding search functionility

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ menuBtn.addEventListener('click', function() {
 closeBtn.addEventListener('click', function() {
     mobileMenu.classList.add('translate-x-full');
 });
+
+//Adding search Functionality
+
+var search=document.getElementById("search")
+var productlist=document.querySelectorAll(".productlist")
+ search.addEventListener("keyup",function()
+    {
+        var enteredValue = event.target.value.toUpperCase()
+        for(var i=0;i<productlist.length;i++)
+        {
+            console.log(productlist)
+          var productname=productlist[i].querySelector("h1").textContent
+          if(productname.toUpperCase().indexOf(enteredValue)<0)
+          {
+            productlist[i].style.display="none"
+          }
+          else
+          {
+            productlist[i].style.display="block"
+          }
+
+        }
+    }
+)
+
+
